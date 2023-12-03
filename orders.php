32013afc575d4ee461d456c4f77460151370b373
9rey-m477er <90400873+9rey-m477er@users.noreply.github.com>
2023-12-03 01:02:19 -0500

    Add files via upload

--- orders.php
+++ orders.php
@@ -3,39 +3,26 @@
 <body>
 
 <?php
-$txt = "PHP";
-
-echo "<h1> I love $txt!</h1>";
-echo "here is the dollar sign - $";
-echo "\n\n";
-
-for ($i = 0; $i < 10; $i++)
-{
-	if($i % 2)
-	echo "$i </br>";
-    else
-    	echo "<b>$i</b><br>";
-}
-for ($i = 0; $i < 6; $i++)
-{
-	echo "<h$i> This is heading $i</h$i>";
-}
-
-$colors = array("Black","White", "Green", "Blue");
-foreach($colors as $n){
-echo "Is $n your favorite color? <br>";
-}
-function show($name, $color){
-	echo "The name $name reminds me of $color <br>";
-	}
-show("Stephen", "Green");
-
-function add(int $x, int $y){
-$z = $x + $y;
-return $z;
-}
-echo "5+7=";
-echo add(5,7);
+    $serverName = "spangili.database.windows.net";
+    $connectionOptions = array(
+        "Database" => "your_database",
+        "Uid" => "projectManager",
+        "PWD" => "jBD9t6E8f!IY"
+    );
+    //Establishes the connection
+    $conn = sqlsrv_connect($serverName, $connectionOptions);
+    $tsql= "SELECT TOP 20 pc.Name as CategoryName, p.name as ProductName
+         FROM [SalesLT].[ProductCategory] pc
+         JOIN [SalesLT].[Product] p
+         ON pc.productcategoryid = p.productcategoryid";
+    $getResults= sqlsrv_query($conn, $tsql);
+    echo ("Reading data from table" . PHP_EOL);
+    if ($getResults == FALSE)
+        echo (sqlsrv_errors());
+    while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
+     echo ($row['CategoryName'] . " " . $row['ProductName'] . PHP_EOL);
+    }
+    sqlsrv_free_stmt($getResults);
 ?>
 
 </body>
